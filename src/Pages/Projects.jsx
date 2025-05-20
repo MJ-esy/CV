7529f417f975bbb7563261e6913b4b4c66a1f018
MJ Eng <mj.eng.contact@gmail.com>
2025-05-20 20:16:19 +0200

    update CSS style

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,72 +1,78 @@
-import { useEffect, useState } from "react"
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 import CircleLoader from "react-spinners/CircleLoader";
-import ProjectPic from '../assets/pietro-jeng-n6B49lTx7NM-unsplash.jpg';
-
+import ProjectPic from "../assets/pietro-jeng-n6B49lTx7NM-unsplash.jpg";
 
 export default function Projects() {
-    const [projects, setProjects] = useState();
-    const [loading, setLoading] = useState(true);
+  const [projects, setProjects] = useState();
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        fetch("https://api.github.com/users/MJ-Eng-codes/repos")
-            .then((response) => response.json())
-            .then((data) => {
-                setTimeout(() => {
-                    setProjects(data);
-                    setLoading(false);
-                    console.log(data);
-                }, 2500)
-            })
-            .catch((error) => { console.log(error) })
-    }, []);
+  useEffect(() => {
+    fetch("https://api.github.com/users/MJ-esy/repos")
+      .then((response) => response.json())
+      .then((data) => {
+        setTimeout(() => {
+          setProjects(data);
+          setLoading(false);
+          console.log(data);
+        }, 2500);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-    if (loading) {
-        return (
-            <>
-                <main className="project-page">
-
-                    <h1 className="p-h1"> Projects</h1>
-                    <div className="project-section">
-                        <CircleLoader
-                            color="#f0ffff"
-                            size={150}
-                        />
-                    </div>
-
-                </main>
-            </>
-
-        )
-    }
-
+  if (loading) {
     return (
-        <>
-            <main className="project-page">
-                <h1 className="p-h1">Projects</h1>
-                <div className="project-section">
-                    {projects.map((project) => {
-                        const formattedDate = new Date(project.created_at).toISOString().split('T')[0];
-                        return (
-                            <Card style={{ width: '18rem' }} key={project.name}>
-                                <Card.Img variant="top" src={ProjectPic} alt="Textured pattern green and red" />
-                                <Card.Body>
-                                    <Card.Title>
-                                        {project.name}
-                                    </Card.Title>
-                                    <Card.Text>
-                                        Language: {project.language || "Unknown"}
-                                        <br />
-                                        Date of creation: {formattedDate}
-                                    </Card.Text>
-                                    <Button className="p-button" variant="primary" href={project.html_url} target="GitHub">More Info</Button>
-                                </Card.Body>
-                            </Card>
-                        )
-                    })}
-                </div>
-            </main>
-        </>
-    )
+      <>
+        <main className="project-page">
+          <h1 className="p-h1"> Projects</h1>
+          <div className="project-section">
+            <CircleLoader color="#f0ffff" size={150} />
+          </div>
+        </main>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <main className="project-page">
+        <h1 className="p-h1">Projects</h1>
+        <div className="project-section">
+          {projects.map((project) => {
+            const formattedDate = new Date(project.created_at)
+              .toISOString()
+              .split("T")[0];
+            return (
+              <Card style={{ width: "18rem" }} key={project.name}>
+                <Card.Img
+                  variant="top"
+                  src={ProjectPic}
+                  alt="Textured pattern green and red"
+                />
+                <Card.Body>
+                  <Card.Title>{project.name}</Card.Title>
+                  <Card.Text>
+                    Language: {project.language || "Unknown"}
+                    <br />
+                    Date of creation: {formattedDate}
+                  </Card.Text>
+                  <Button
+                    className="p-button"
+                    variant="primary"
+                    href={project.html_url}
+                    target="GitHub"
+                  >
+                    More Info
+                  </Button>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </div>
+      </main>
+    </>
+  );
 }
--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -1,88 +1,88 @@
-import { useEffect } from 'react';
-import Stack from 'react-bootstrap/Stack';
+import { useEffect } from "react";
+import Stack from "react-bootstrap/Stack";
 import education from "../data/education.json";
 import employments from "../data/employments.json";
 import language from "../data/language.json";
 
+export default function Resume() {
+  return (
+    <>
+      <main>
+        <h1>My Resumé</h1>
+        <div className="p-resume">
+          <Stack gap={4}>
+            <div className="p-2">
+              <h2>Employment</h2>
+              <br />
+              <br />
+              <ul className="employment">
+                {employments.map((employment, index) => {
+                  return (
+                    <li key={index}>
+                      <h4>{employment.Position}</h4>
+                      <h5>
+                        {employment.Company} @{employment.Duration}
+                      </h5>
+                      <p>{employment["Job-description"]}</p>
+                      <br />
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            <div className="p-2">
+              <h2>Education</h2>
+              <br />
+              <br />
+              <ul className="education">
+                {education.map((education, index) => {
+                  return (
+                    <li key={index}>
+                      <h4>{education.Course}</h4>
+                      <h5>
+                        {education.School} <br />
+                        {education.Duration}
+                      </h5>
+                      <p>{education.Info}</p>
+                      <br />
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            <div className="p-2">
+              <h2>Language</h2>
+              <br />
+              <br />
+              <ul className="language">
+                {language.map((language, index) => {
+                  return (
+                    <li key={index}>
+                      <h4>{language.language}</h4>
+                      <p>{language.proficiency}</p>
+                      <br />
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
 
-export default function Resume(){
-   
-    return(
-        
-        <>
-        <main>
-            <h1>My Resumé</h1>
-                <Stack gap={4}>
-                    <div className="p-2" >
-                        <h2>Employment</h2>
-                        <br /><br />
-                        <ul className='employment'>
-                        {employments.map((employment, index) =>{
-                            return(
-                                <li key={index}> 
-                                <h4>{employment.Position}</h4>
-                                <h5>{employment.Company} @{employment.Duration}</h5>
-                                <p>{employment["Job-description"]}</p>
-                                <br />
-                            </li>    
-                            )})}
-                        </ul>
-
-
-                    </div>
-                    <div className="p-2">
-                        <h2>Education</h2>
-                        <br /><br />
-                        <ul className='education'>
-                        {education.map((education, index) =>{
-                            return(
-                                <li key={index}> 
-                                <h4>{education.Course}</h4>
-                                <h5>{education.School} <br />{education.Duration}</h5>
-                                <p>{education.Info}</p>
-                                <br />
-                            </li>
-                            
-                            )})}
-                        </ul>
-
-                    </div>
-                    <div className="p-2">
-                        <h2>Language</h2>
-                        <br /><br />
-                        <ul className='language'>
-                        {language.map((language, index) =>{
-                            return(
-                                <li key={index}> 
-                                <h4>{language.language}</h4>
-                                <p>{language.proficiency}</p>
-                               <br />
-                            </li>    
-                            )})}
-                        </ul>
-                    </div>
-
-                    <div className="p-2">
-                        <h2>Skills</h2>
-                        <br /><br />
-                        <ul className='skills'>
-                            <li>C#</li> 
-                            <li>.NET</li>
-                            <li>SQL</li>
-                            <li>JavaScript</li>
-                            <li>React</li>
-                            <li>HTML & CSS</li>
-
-                        </ul>
-
-                    </div>
-
-                </Stack>
-
-        </main>
-             
-  
-        </>
-
-    )
+            <div className="p-2">
+              <h2>Skills</h2>
+              <br />
+              <br />
+              <ul className="skills">
+                <li>C#</li>
+                <li>.NET</li>
+                <li>SQL</li>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>HTML & CSS</li>
+              </ul>
+            </div>
+          </Stack>
+        </div>
+      </main>
+    </>
+  );
 }
