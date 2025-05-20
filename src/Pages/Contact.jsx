c469beca2d90c014469f3741e4082146ded86b30
MJ Eng <mj.eng.contact@gmail.com>
2025-05-20 22:02:36 +0200

    Add more info

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,21 +1,18 @@
-import ContactPic from '../assets/birds.jpg';
+import ContactPic from "../assets/birds.jpg";
 
 export default function Contact() {
+  return (
+    <>
+      <main className="Contact">
+        <img src={ContactPic} alt="birds in a row" />
 
-    return (
-
-        <>
-            <main className="Contact">
-                <img src={ContactPic} alt="birds in a row" />
-
-                <div className="p-contact">
-                    <p>Looking to collaborate or share ideas?</p>
-                    <p>Drop me a message! 😊 I’d love to connect! </p>
-                </div>
-
-            </main>
-
-        </>
-
-    )
+        <div className="p-contact">
+          <p>Looking to collaborate or share ideas?</p>
+          <p>Drop me a message! 😊 I’d love to connect! </p>
+          <a href="https://github.com/MJ-esy">GitHub</a> &nbsp;
+          <a href="www.linkedin.com/in/mj-yen-eng">LinkedIn</a>
+        </div>
+      </main>
+    </>
+  );
 }
